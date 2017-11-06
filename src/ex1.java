33d0d6d6fbea5b5a9d77c6811aa0e71baff43b2f
kruf <falahi161212@bscse.uiu.ac.bd>
2017-11-07 00:48:35 +0600

    check

---
+++ src/ex1.java
@@ -0,0 +1,16 @@
+public class ex1 {
+
+    public static void main(String []args)
+    {
+        vector_2d v=new vector_2d(1,2);
+
+        System.out.println(v.dis());
+        System.out.println(v);
+
+        vector_3d v4=new vector_3d(0,6,8);
+
+        System.out.println(v4.dis());
+        System.out.println(v4);
+    }
+
+}
