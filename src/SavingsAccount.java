f1ce75a39caeafa2f2f71df5936670e77ac05bb7
kruf <falahi161212@bscse.uiu.ac.bd>
2017-11-10 01:30:47 +0600

    SavingAccounts final

---
+++ src/SavingsAccount.java
@@ -0,0 +1,29 @@
+public class SavingsAccount extends BankAccount{
+    double interest;
+    int maxWithdraw;
+    SavingsAccount(String _memberName,int _accountBalance,int _minimumbalance)
+    {
+        super(_memberName,_accountBalance,_minimumbalance);
+        interest=.5;
+    }
+    double getBalanceWithinterest()
+    {
+        double interest=accountBalance*this.interest+accountBalance;
+        return interest;
+    }
+
+    public void setMaxWithdraw(int maxWithdraw) {
+        this.maxWithdraw = maxWithdraw;
+    }
+
+
+
+    String withdraw(double withdraw) {
+        if(maxWithdraw>=withdraw)
+        {
+            accountBalance=accountBalance-withdraw;
+            return ("withdraw successful");
+        }
+        return ("exceed withdraw limit");
+    }
+}
